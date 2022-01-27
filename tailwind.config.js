f1f59576228ee97f6cdf679563304623c9f67449
leon-kino <muyelingyin@gmail.com>
2022-01-27 21:18:55 +0900

    skills部分の実装

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,17 @@ module.exports = {
       'light-green': '#008c8d',
       'bgc-green': '#6bc2c3',
       'line-green': '#c5eaea',
-      'white':'#ffffff',
-      'sub-title':'#dcf4f6',
-      'brack':'#384359',
+      'white': '#ffffff',
+      'sub-title': '#dcf4f6',
+      'brack': '#384359',
+      'bgc-skills': '#C6EAEA',
+      'skills-bar': '#2BB9BB',
     },
     backgroundImage: {
-        'about': "url('/bg-about.png')",
+      'about': 'url("/bg-about.png")',
+      'skills': 'url("/bg-skills.png")',
     },
     extend: {},
   },
   plugins: [],
-}
+};
