52e110294bb885dd774fef1e4357eee25ac20743
leon-kino <muyelingyin@gmail.com>
2022-02-16 16:38:44 +0900

    skills部分のバーが表示されるように変更

--- components/skills.tsx
+++ components/skills.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Skills = (props: { title: string; percent: number}) => {
   const array =
-   ['1/12', '1/5', '2/6', '2/5', '1/2', '3/5', '4/6', '4/5', '11/12', 'full'];
+   ['w-1/12', 'w-1/5', 'w-2/6', 'w-2/5',
+     'w-1/2', 'w-3/5', 'w-4/6', 'w-4/5', 'w-11/12', 'w-full'];
   const per =
-   'absolute bg-skills-bar h-4 w-' +
+   'absolute bg-skills-bar h-4 ' +
     array[props.percent / 10 - 1] + ' borders rounded';
   return (
     <>
