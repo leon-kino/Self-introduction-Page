60857726be9004f4b48a51c8efb47beed50d698b
leon-kino <muyelingyin@gmail.com>
2022-02-28 20:26:22 +0900

    ogpを設定

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,7 @@ import Nav from './Nav';
 const Header = () => {
   return (
     <div className="w-full h-full md:flex md:justify-between">
+      <img src="/ogp/ogp.png" className="w-0 h-0" />
       <img src="/logo.png" alt="Fwywd Logo" className="w-40 h-20 m-auto md:ml-0" />
       <ul className="h-full flex justify-center mb-5 md:my-auto">
         <Nav title={'ABOUT'} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,40 @@ import Hero from '../components/Hero';
 import AboutSkills from '../components/AboutSkills';
 import ValuesFuture from '../components/ValuesFuture';
 import { NextSeo } from 'next-seo';
+import { GetStaticProps } from 'next';
+import createOgp from '../utis/server/ogpUtils';
 
 const Home = () => {
+  const getStaticProps: GetStaticProps = async () => {
+    [...Array(10)].forEach((_) => {
+      void createOgp();
+    });
+
+    return {
+      props: {},
+    };
+  };
+  getStaticProps;
   return (
     <>
       <NextSeo title="自己紹介ページ" description="木野伶音の自己紹介ページです。" />
       {/* head */}
       <Head>
         <title>自己紹介ページ</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          property="og:image"
+          key="ogImage"
+          content={`https://self-introduction-page.vercel.app/`}
+        />
+        <meta name="twitter:card" key="twitterCard" content="summary_large_image" />
+        <meta name="twitter:title" content={'aaaa'} />
+        <meta name="twitter:description" content={'これはテストだよ'} />
+        <meta
+          name="twitter:image"
+          key="twitterImage"
+          content={`https://self-introduction-page.vercel.app/`}
+        />
       </Head>
       {/* body */}
       <Hero />
