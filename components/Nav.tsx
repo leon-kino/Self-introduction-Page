f21d115f83fd65f9211f5422095b83a3f6f4d9a7
leon-kino <muyelingyin@gmail.com>
2022-03-01 09:23:47 +0900

    スムーススクロールを実装

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Nav = (props: { title: string }) => {
+  const src = '#' + props.title;
   return (
     <li className="mx-3 text-light-green font-bold">
-      <a href="#">{props.title}</a>
+      <a href={src}>{props.title}</a>
     </li>
   );
 };
